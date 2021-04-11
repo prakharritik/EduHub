1b91bd45bd23c8f4adefa696ef25de6d6aafc653
prakharritik <60786008+prakharritik@users.noreply.github.com>
2021-04-11 13:59:14 +0530

    missing files

---
+++ script/details.js
@@ -0,0 +1,39 @@
+document.querySelector('#boxIdS').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_m').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_V').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_E').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_M').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_J').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_S').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_U').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_N').addEventListener('mouseenter',()=>{
+	location.href="";
+});
+
+document.querySelector('#boxId_P').addEventListener('mouseenter',()=>{
+	location.href="";
+});
---
+++ script/solarsystem.js
@@ -0,0 +1,37 @@
+
+document.querySelector('#sunid').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="sun.html"},800);
+});
+
+document.querySelector('#Mercury_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="mercury.html"},800);
+});
+
+document.querySelector('#Venus_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="venus.html"},800);
+});
+
+document.querySelector('#Earth_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="earth.html"},800);
+});
+
+document.querySelector('#Mars_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="mars.html"},800);
+});
+
+document.querySelector('#Jupiter_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="jupiter.html"},800);
+});
+
+document.querySelector('#Saturn_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="saturn.html"},800);
+});
+
+document.querySelector('#Uranus_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="uranus.html"},800);
+});
+
+document.querySelector('#Neptune_id').addEventListener('mouseenter',()=>{
+	setTimeout(()=>{location.href="neptune.html"},800);
+});
+

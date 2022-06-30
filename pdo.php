85039502a3a619edf0304210b7f48e182c842f52
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-06-30 11:04:15 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -14,9 +14,9 @@
     //$DB_NAME = 'education';
     //$DB_USER = 'root';
     //$DB_PASSWORD = '';
-    $DB_NAME = getenv(host);
-    $DB_USER = getenv(host);
-    $DB_PASSWORD = getenv(pass);
+    $DB_NAME = getenv('host');
+    $DB_USER = getenv('host');
+    $DB_PASSWORD = getenv('pass');
     $pdo = new PDO(
         "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
         //"pgsql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
